8944ad1f52b7ac36345b9afbef55fb509e1dc669
millouu <srishti.chaurasia123@gmail.com>
2023-03-19 23:24:30 +0530

    displaying task component

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-task',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent {
-
+  @Input() task: Task | null = null;
+  @Output() edit=new EventEmitter<Task>()
 }

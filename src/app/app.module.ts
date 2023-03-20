e290a7a584b4dddbea0e1dcaa048865b8a4cbfb3
millouu <srishti.chaurasia123@gmail.com>
2023-03-20 11:01:14 +0530

    installing angular cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
